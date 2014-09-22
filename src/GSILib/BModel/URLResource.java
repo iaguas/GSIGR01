2ed9b3d048831f619a5fd7b3d994abff7ae0b0c0
linux1 <linux1@localhost>
2014-09-22 18:39:58 +0200

    Introducido esqueleto de la clase URLResource (interface de clases WebNews y Picture)

--- src/GSILib/BModel/URLResource.java
+++ src/GSILib/BModel/URLResource.java
@@ -5,10 +5,23 @@
  */
 package GSILib.BModel;
 
+import GSILib.BModel.Picture;
+import GSILib.BModel.documents.visualNews.WebNews;
+/*import GSILib.BModel.workers.Journalist;*/
+import GSILib.BModel.workers.Photographer;
+
 /**
  *
- * @author inigo.aguas
+ * @author inigo.aguas & Iñaki Garcia
  */
 public interface URLResource {
+    /* Constructor y método de WebNews*/   
+    /*public WebNews(String headline, String body, Journalist journalist, String url);*/
+    public boolean addKeyWord(String keyword);
     
+    /* Constructor y métodos de Picture */
+    /*public Picture(String url, Photographer photographer);*/
+    // Get privates
+    public String getUrl();
+    public Photographer getAutor();    
 }
