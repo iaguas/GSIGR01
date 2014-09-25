d5c3bcd072f6bbf7e51949351fe5173e9909d55f
Alvaroctal <Alvaro@Macintosh.local>
2014-09-25 13:27:48 +0200

    Comit de darle en las narices a aguas
    - Bocazas

--- src/GSILib/BModel/WebResource.java
+++ src/GSILib/BModel/WebResource.java
@@ -14,6 +14,9 @@ import GSILib.BModel.workers.Photographer;
  *
  * @author inigo.aguas & Iñaki Garcia
  */
+
+// Comit hecho, aguas eres un boca tunel
+
 public interface WebResource {
     /* Constructor y método de WebNews*/   
     /*public WebNews(String headline, String body, Journalist journalist, String url);*/
