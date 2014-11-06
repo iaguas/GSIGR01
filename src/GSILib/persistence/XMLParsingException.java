53a27e377cd7816402385e028fd0e5d1d67f0f1a
linux1 <linux1@localhost>
2014-11-06 10:57:10 +0100

    Primera implementación XMLParsingException

---
+++ src/GSILib/persistence/XMLParsingException.java
@@ -0,0 +1,25 @@
+/* TODO: javadoc y comentarios */
+
+/*
+ * Esto es una prueba
+ * Each line should be prefixed with  * 
+ */
+
+package GSILib.persistence;
+
+/**
+ *
+ * @author linux1
+ */
+public class XMLParsingException extends Exception{
+    
+    public XMLParsingException(String message) {
+        super(message);
+    }
+
+    public XMLParsingException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+    
+    
+}
