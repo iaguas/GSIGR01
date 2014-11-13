9a2be2c856653d5e8d904854a8d30e8db9837b90
linux1 <linux1@localhost>
2014-11-13 11:18:01 +0100

    Segunda implementación de XMLParsingException
    - Añadidos métodos que comprueban la ejecución de los métodos que analizan los XML
    - Falta por asegurar la correcta integración

--- src/GSILib/persistence/XMLParsingException.java
+++ src/GSILib/persistence/XMLParsingException.java
@@ -7,11 +7,31 @@
 
 package GSILib.persistence;
 
+import java.io.IOException;
+import java.io.StringReader;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+/**
+ * TODO: Comentar esta clase
+ */
 /**
  *
  * @author linux1
  */
 public class XMLParsingException extends Exception{
+    
+    // constructor
+    /*public XMLParsingException() {
+
+    }*/
     
     public XMLParsingException(String message) {
         super(message);
@@ -21,5 +41,57 @@ public class XMLParsingException extends Exception{
         super(message, throwable);
     }
     
+    /**
+     * Getting XML DOM element
+     * @param xml string
+     * @return 
+     * */
+    public Document getDomElement(String xml){
+        Document doc = null;
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        try {
+
+            DocumentBuilder db = dbf.newDocumentBuilder();
+
+            InputSource is = new InputSource();
+                is.setCharacterStream(new StringReader(xml));
+                doc = db.parse(is); 
+
+            } catch (ParserConfigurationException | SAXException | IOException e) {
+                //Log.e("Error: ", e.getMessage());
+                e.getMessage();
+                return null;
+            }
+
+            return doc;
+    }
+    
+     /** Getting node value
+      * @param elem element
+      */
+    public final String getElementValue( Node elem ) {
+        Node child;
+        if( elem != null){
+            if (elem.hasChildNodes()){
+                for( child = elem.getFirstChild(); child != null; child = child.getNextSibling() ){
+                    if( child.getNodeType() == Node.TEXT_NODE  ){
+                        return child.getNodeValue();
+                    }
+                }
+            }
+        }
+        return "";
+    }
+
+    /**
+     * Getting node value
+     * @param item node
+     * @param str string
+     * @return 
+     * */
+    public String getValue(Element item, String str) {     
+           NodeList n = item.getElementsByTagName(str);        
+           return this.getElementValue(n.item(0));
+    }
     
 }
