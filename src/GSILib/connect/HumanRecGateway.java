174433da1f886847b42697b0b4541d2e60de2ca8
iaguas <aguas.71332@e.unavarra.es>
2014-11-20 10:59:50 +0100

    Interfaces P4

---
+++ src/GSILib/connect/HumanRecGateway.java
@@ -0,0 +1,49 @@
+package GSILib.connect;
+
+import GSILib.BModel.Journalist;
+import GSILib.BModel.Photographer;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ *  This interface represents the access HR people has to the BusinessSystem.
+ * @author carlos.lopez
+ */
+public interface HumanRecGateway extends Remote {
+
+    /**
+     * Adds a new worker to the System
+     * @param jn The new journalist
+     * @return  Boolean if and only if it could be added and did not exist in the system before.
+     * @throws RemoteException If something goes wrong in the connection/serialization.
+     */
+    public Boolean addWorker(Journalist jn) throws RemoteException;
+    
+    /**
+     * Adds a new worker to the System
+     * @param pg The new journalist
+     * @return  Boolean if and only if it could be added and did not exist in the system before.
+     * @throws RemoteException  If something goes wrong in the connection/serialization.
+     */
+    public Boolean addWorker(Photographer pg)  throws RemoteException;;
+    
+    /**
+     * Updates the information about a worker to the system. The journalist with the same ID is
+     *      replaced by the new one, although it remains author of his pictures, etc.
+     * @param jn The new version of the journalist
+     * @return  Boolean True if and only if the journalist existed in the system before and could be replaced.
+     * @throws RemoteException  If something goes wrong in the connection/serialization.
+     */    
+    public Boolean updateWorker(Journalist jn)  throws RemoteException;;
+    
+    /**
+     * Updates the information about a worker to the system. The photographer with the same ID is
+     *      replaced by the new one, although it remains author of his pictures, etc.
+     * @param pg The new version of the photographer
+     * @return  Boolean True if and only if the photographer existed in the system before and could be replaced.
+     * @throws RemoteException  If something goes wrong in the connection/serialization.
+     */    
+    public Boolean updateWorker(Photographer pg)  throws RemoteException;;
+    
+    
+}
---
+++ src/GSILib/connect/ValidationGateway.java
@@ -0,0 +1,53 @@
+
+package GSILib.connect;
+
+import GSILib.BModel.Journalist;
+import GSILib.BModel.PrintableNews;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * This inteface offers an acces for listing and validating news in our Editorial System.
+ * @author carlos.lopez
+ */
+public interface ValidationGateway extends Remote{
+    
+    /**
+     * Lists all the printable news in the system which have not been reviewed by
+     *  the minimum number of reviewers.
+     * @return The list of PrintableNews, in no specific order.
+     * @throws RemoteException If something goes wrong in the connection/serialization.
+     */
+    public PrintableNews[] getPendingNews() throws RemoteException;
+    
+    /**
+     * Lists all the printable news in the system which have been reviewed by,
+     *  at most, maxReviewers.
+     * @param numReviewers Maximum number of reviewers in the system.
+     * @return The list of PrintableNews, in no specific order.
+     * @throws RemoteException If something goes wrong in the connection/serialization.
+     */
+    public PrintableNews[] getPendingNews(Integer numReviewers)  throws RemoteException;
+   
+    
+    /**
+     * Uploads a revised version of the news, which replaces the old one. The
+     *  list of reviewers is then cleared (set to empty).
+     * @param pn    The new version of the news
+     * @return  True if and only if the operation could be completed.
+     * @throws RemoteException If something goes wrong in the connection/serialization.
+     */
+    public boolean correctNews(PrintableNews pn)  throws RemoteException;
+    
+    /**
+     * Adds the Journalist jn as reviewer in the PrintableNews pn
+     * @param pn    The PrintableNews that has been reviewed by the journalist
+     * @param jn    The journalist him/herself
+     * @return  True if and only if the operation could be completed, the PrintableNews exists,
+     *          it has not been modified and the journalist was not listed as reviewer already.
+     * @throws RemoteException If something goes wrong in the connection/serialization.
+     */
+    public boolean validateNews(PrintableNews pn,Journalist jn)  throws RemoteException;
+
+    
+}
